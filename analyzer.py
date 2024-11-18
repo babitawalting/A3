73b3189ffe9be3fe07da9fbd18ddf5d63eebe8ec
bwalting <babita.alting@hotmail.com>
2024-11-18 09:15:31 +0100

    start-menu aangemaakt

---
+++ analyzer.py
@@ -0,0 +1,11 @@
+#start-menu
+
+print("Kies een optie:")
+print("1. Gemiddelde salaris")
+print("2. Gemiddelde salaris van functie X")
+print("3. Aantal werknemers binnen 2 jaar met pensioen")
+print("4. Aantal chauffeurs")
+print("5. Top 10 langst in dienst")
+print("6. Aantal medewerkers met functie X bij afdeling Y")
+print("W. Alle info naar bestand")
+print("X. Exit")
