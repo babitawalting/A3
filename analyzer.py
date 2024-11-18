3b235e5d7a968361405dc46c9de55b381e95dd79
bwalting <babita.alting@hotmail.com>
2024-11-18 09:27:50 +0100

    comments

--- analyzer.py
+++ analyzer.py
@@ -16,3 +16,13 @@ print("6. Aantal medewerkers met functie X bij afdeling Y")
 print("W. Alle info naar bestand")
 print("X. Exit")
 
+#gemiddeld salaris
+#gemiddeld salaris voor functie X
+#aantal werknemers binnen 2 jaar met pensioen
+
+#aantal chauffeurs
+#top 10 langst in dienst
+#aantal medewerkers met functie X bij afdeling Y
+
+#export (w)
+#exit (x)
