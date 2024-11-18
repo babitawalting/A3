bcc2233c181737c47238d472548a6c8e845121e8
Annvka <D273734@edu.curio.nl>
2024-11-18 10:02:08 +0100

    Functie 1 : gemiddelde

--- analyzer.py
+++ analyzer.py
@@ -7,7 +7,7 @@ vwList = list(reader)
 
 #start-menu
 print("Keuzemenu:")
-print("1. Gemiddelde salaris")
+print("1. Gemiddeld salaris")
 print("2. Gemiddelde salaris van functie X")
 print("3. Aantal werknemers binnen 2 jaar met pensioen")
 print("4. Aantal chauffeurs")
@@ -15,21 +15,26 @@ print("5. Top 10 langst in dienst")
 print("6. Aantal medewerkers met functie X bij afdeling Y")
 print("W. Alle info naar bestand")
 print("X. Exit")
-isRunning = True
-inputNumber = input("Kies een nummer uit het menu: ")
 
+isRunning = True
+choice = input("Kies een nummer uit het menu: ")
 
 #gemiddeld salaris
-totalIncome = 0
-averageIncome = 0
 listLength = 0
 listLength = len(vwList)
+totalIncome = 0
+averageIncome = 0
 
-for Salaris_bruto in vwList:
-    totalIncome += float(Salaris_bruto["Salaris_bruto"])
-    averageValue = totalIncome / listLength
+if choice =="1":
+    for Salaris_bruto in vwList:
+        totalIncome += int(Salaris_bruto["Salaris_bruto"])
+        averageIncome = totalIncome / listLength
 
+    averageIncome = round(averageIncome)
+    print(f"Gemiddeld salaris van alle werknemers:\n{averageIncome} euro")
+    
 #gemiddeld salaris voor functie X
+
 #aantal werknemers binnen 2 jaar met pensioen
 
 #aantal chauffeurs
